4d1c93feeec74d7e3533943da64e5988105fe316
Fekete Gyorgy <f@pulilab.com>
2013-09-19 19:26:12 +0200

    TearDownMixin for deleting test files

--- medialibrary/tests/__init__.py
+++ medialibrary/tests/__init__.py
@@ -1,3 +1,19 @@
+import shutil
+
+
+class TearDownMixin(object):
+    """
+    This mixin removes all the test files (and folders) created by the tests
+    """
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            shutil.rmtree('test_temp')
+        except OSError:
+            pass
+
+
 from models import *
 from serializers import *
 from views import *
